2d752a6f7e87947893cb51b70ee454a2867a4ffa
Michelangelo Setaro <mksetaro@gmail.com>
2026-09-02 10:09:47 +0200

    feat(github): restore pull request tools and prune stale crew docs

--- .pi/extensions/github/index.ts
+++ .pi/extensions/github/index.ts
@@ -3,6 +3,7 @@ import { registerIssueTools } from "./lib/issues/tools.ts";
 import { registerProjectTools } from "./lib/projects/tools.ts";
 import { registerLabelTools } from "./lib/labels/tools.ts";
 import { registerCoreTools } from "./lib/core/tools.ts";
+import { registerPullRequestTools } from "./lib/pulls/tools.ts";
 
 export default function (pi) {
   registerDiscussionTools(pi);
@@ -10,4 +11,5 @@ export default function (pi) {
   registerProjectTools(pi);
   registerLabelTools(pi);
   registerCoreTools(pi);
+  registerPullRequestTools(pi);
 }
